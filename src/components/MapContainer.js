4a626b760ec41ebac968aea2e5f69394376c2773
Elie Saab <esaab@dcgroup.com>
2022-05-23 22:16:16 +0300

    fixed map rerender

--- src/components/MapContainer.js
+++ src/components/MapContainer.js
@@ -1,26 +1,36 @@
-import { useRef, useEffect } from "react";
+import { useRef, useEffect, forwardRef, useImperativeHandle } from "react";
 import Map from "@arcgis/core/Map";
 import MapView from "@arcgis/core/views/MapView";
 import config from "@arcgis/core/config";
 import "../App.css";
 
-export default function MapContainer({ center }) {
-  config.apiKey =
-    "AAPK1cfdb9758bad43cab47edaefc9a1b686j5LIzscHXKAgqloA7svZpG0z1Zzo133MdUipEqjscC8J3oN_670XHF2AXhLJqgKX";
+const MapContainer = forwardRef(({ center }, ref) => {
+  // config.apiKey =
+  //   "AAPK1cfdb9758bad43cab47edaefc9a1b686j5LIzscHXKAgqloA7svZpG0z1Zzo133MdUipEqjscC8J3oN_670XHF2AXhLJqgKX";
   const mapDiv = useRef(null);
+  const map = new Map();
+  const view = new MapView();
+
+  useImperativeHandle(ref, () => ({
+    setNewLocation(coord) {
+      console.log("zooming...");
+      console.log(coord);
+      if (Array.isArray(coord))
+        view.goTo({
+          center: coord,
+        });
+    },
+  }));
 
   useEffect(() => {
+    console.log("render");
     if (mapDiv.current) {
-      const map = new Map({
-        basemap: "arcgis-topographic",
-      });
+      map.basemap = "topo-vector";
+      view.map = map;
+      view.center = center;
+      view.zoom = "7";
+      view.container = mapDiv.current;
 
-      const view = new MapView({
-        map: map,
-        center: center,
-        zoom: 7,
-        container: mapDiv.current,
-      });
       if (Array.isArray(center))
         view.goTo({
           center: center,
@@ -29,4 +39,6 @@ export default function MapContainer({ center }) {
   });
 
   return <div id="viewDiv" ref={mapDiv}></div>;
-}
+});
+
+export default MapContainer;
