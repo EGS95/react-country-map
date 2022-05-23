9da6f180a99e8da20d425a47761431c65d477da3
Elie Saab <esaab@dcgroup.com>
2022-05-23 22:14:51 +0300

    added mapcontainer ref

--- src/App.js
+++ src/App.js
@@ -1,19 +1,21 @@
-import { useState } from "react";
+import { useState, useRef } from "react";
 import Sidebar from "./components/Sidebar";
 import MapContainer from "./components/MapContainer";
 import "./App.css";
 
 function App() {
+  const mapRef = useRef();
   const [center, setCenter] = useState([35.8623, 33.8547]);
 
-  const zoomToMap = (coodrdinates) => {
-    setCenter(coodrdinates);
+  const zoomToMap = (coordinates) => {
+    // setCenter(coodrdinates);
+    mapRef.current.setNewLocation(coordinates);
   };
 
   return (
     <div id="container">
       <Sidebar zoomToMap={zoomToMap} />
-      <MapContainer center={center} />
+      <MapContainer center={center} ref={mapRef} />
     </div>
   );
 }
